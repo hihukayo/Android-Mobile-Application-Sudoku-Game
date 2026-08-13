8cb9caaa9dbc2532b8b49e1e1134a3f453109f81
hihukayo <Hihukayo@outlook.com>
2026-08-13 16:23:10 +0800

    chore: 版本号升至 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.sudoku"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     buildTypes {
